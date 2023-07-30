509b4cbb29fda312d74ea5f60d1dfd51ebb9d6d4
wpeckelsen <wpeckelsen@gmail.com>
2023-07-30 22:37:56 +0200

    security removed

--- src/main/java/dk/cs/domaindb/Security/auth/AuthKey.java
+++
@@ -1,8 +0,0 @@
-package dk.cs.domaindb.Security.auth;
-
-import java.io.Serializable;
-
-public class AuthKey implements Serializable {
-    private String username;
-    private String authority;
-}
